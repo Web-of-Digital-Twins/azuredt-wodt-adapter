d79a4915735688d976c1721572b62f468435a0a2
Andrea Giulianelli <andrygiulio@gmail.com>
2024-11-27 12:31:25 +0100

    chore: model dtd manager component

---
+++ src/main/kotlin/application/component/DtdManager.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024. Andrea Giulianelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package application.component
+
+import org.eclipse.ditto.wot.model.ThingDescription
+import java.net.URI
+
+/** This interface models the DTD Manager component. */
+interface DtdManager {
+    /**
+     * Obtain the Digital Twin Description of the DT with the provided [dtUri].
+     * The adapter implements the DTD with the Thing Description.
+     */
+    operator fun get(dtUri: URI): ThingDescription?
+}
